6e85bf79de827a4b8a5255b76451fb039b6f9b85
buzzdecafe <m_hur@yahoo.com>
2013-07-01 20:18:39 -0400

    initial commit; ruler is sliding

--- app/js/app.js
+++ app/js/app.js
@@ -13,20 +13,65 @@ nucalc.config(['$routeProvider', function($routeProvider) {
   }]);
 
 nucalc.controller("RulerCtrl", function($scope) {
-  $scope.init = "";
+  $scope.prop = "Ruler Controller";
 });
 
 nucalc.directive("ruler", function() {
+  var mid = (window.innerWidth/2 >> 0) + "px";
   return {
-    restrict: "A",
-    scope: { },
-    link: function(scope, element, attrs) {
-      element.bind("mousedown", function($event) {
-        scope.clicked = scope.clicked + 1 || 0
-        console.log(scope);
-        console.log(attrs.direction, scope.clicked);
-      });
-    }
+    restrict: "E",
+    replace: true,
+    scope: { 
+      direction: "@",
+      rid:"@"
+    },
+    compile: function(elem, attrs, xclude) {
+      var dragging, startX, deltaX, bgPos;
+      
+      // align ruler left edge to center
+      elem[0].style.backgroundPositionX = bgPos = mid;
+
+      // return postLink function
+      return function postLink(scope, $element, attrs) {
+        var elem = $element[0];
+
+        function out($event) {
+          dragging = false;
+          bgPos = parseInt(elem.style.backgroundPositionX, 10);
+        }
+
+        $element.bind("mousedown", function($event) {
+          dragging = true;
+          startX = $event.clientX;
+          deltaX = 0;
+        });
+      
+        $element.bind("mousemove", function($event) {
+          if (dragging) {
+            deltaX = $event.clientX - startX;
+            elem.style.backgroundPositionX = (bgPos + deltaX) + "px";        
+          }
+        });
+  
+        $element.bind("mouseup", out);
+        
+        $element.bind("mouseout", out);
+
+      }
+    },
+    templateUrl: "js/partials/ruler.html",
+  };
+});
+
+nucalc.directive("rulerPair", function() {
+  return {
+    restrict: "E",
+    replace: true,
+    scope: {
+      top: "@",
+      bottom: "@"
+    },
+    templateUrl: "js/partials/rulerpair.html"
   };
 });
 
--- app/js/directives/directives.js
+++ app/js/directives/directives.js
@@ -2,18 +2,22 @@
 
 /* Directives */
 
-
 angular.module('nucalc.directives', []).
   directive('ruler', function() {
     return {
       templateUrl: 'views/ruler.html',
       restrict: 'E',
       scope: {
-        direction: "bind",
-        rid: "bind"
+        direction: "@",
+        rid: "@"
       },
-      controller: function($scope, $element, $attrs) {
-        console.log("Hi there!");
+      //controller: function($scope, $element, $attrs) {
+      //  console.log("Hi there!");
+      //},
+      link: function($scope, $element, $attrs) {
+        console.log("$scope", $scope);
+        console.log("$element", $element);
+        console.log("$attrs", $attrs);
       }
     };
   });
